b02ba5e1c896fef262e429c4f393a8d03e310899
Daniel Martines <curshet@mail.ru>
2021-12-03 18:00:58 +0300

    Add class with Navigation Bar settings

---
+++ SM.Weather/Screens/NavigationController/MainScreenNavigationBarSetup.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class MainScreenNavigationBarSetup {
+    
+    public func navigationBarStyle(_ viewController: MainScreenViewController) {
+        viewController.navigationBar.shadowImage = UIImage()
+        addItems(viewController)
+    }
+    
+    private func addItems(_ viewController: MainScreenViewController) {
+        let darkModeItem = UIBarButtonItem(image: UIImage(systemName: "sun.max"),
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(viewController.darkModeButtonAction))
+        darkModeItem.tintColor = .lightGray
+        viewController.navigationBar.topItem?.rightBarButtonItems = [darkModeItem]
+    }
+    
+}
--- SM.Weather/Screens/NavigationController/MainScreenViewController.swift
+++ SM.Weather/Screens/NavigationController/MainScreenViewController.swift
@@ -4,7 +4,7 @@ class MainScreenViewController: UINavigationController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		MainScreenNavBarSetup().navigationBarStyle(self)
+        MainScreenNavigationBarSetup().navigationBarStyle(self)
 	}
 	
 }
